7463381f9eb49c0a4e9c61a763181c908be4c280
Alexander Whan <whan.alexander@gmail.com>
2020-07-19 11:14:11 -0700

    Added explanatory comments and fixed accidental state modificiation error in render method of todo item

--- src/App2Class.js
+++ src/App2Class.js
@@ -14,9 +14,16 @@ class App2Class extends React.Component {
 
     handleChange(id){
         this.setState(prevState => {
+            // Remember: You don't want to directly modify State. So, what we do here is -  Looking at previous state, and creating a new array that almost perfectly mirrors what it used to be, except one of the items (which has the same ID as the function received from the onChange handler) is flipping from true to false or false to true (and then that new array is returned and passed to the state as a property)
+            // Brand new array that will replace our old todosData above and get re-rendered
             const updatedTodos = prevState.todos.map(todo => {
                 if (todo.id === id) {
-                    todo.completed = !todo.completed
+                    // creates a whole new object below with the spread operator - new object is the same EXCEPT it flips true->false or false->true in the "completed" property
+                    // ... = spread operator (gives us all the properties of the todo)
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
                 }
                 return todo
             })
@@ -40,5 +47,8 @@ class App2Class extends React.Component {
         )
     }
 }
+
+// ... = object spread notation
+// saying here: give me all of the properties of this todo item
 
 export default App2Class
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,11 @@ import React from "react"
 
 function TodoItem(props) {
   return (
+    // "checked" property basically says - if property "completed" is true, check it, if false, unchecked
     <div className="todo-item">
       <input type="checkbox" checked={props.item.completed} 
       // empty function that calls props.handleChange and passes the ID to it (ID of the item that was clicked)
+      // Calls onChange when clicked
       onChange={() => props.handleChange(props.item.id)}
       />
       <p>{props.item.text}</p>
