c74ae32d0889ac9e2a96a860370fdef2ed920147
Alexander Whan <whan.alexander@gmail.com>
2020-07-19 11:04:35 -0700

    Created Todo list using changing state and setState method

--- src/App2Class.js
+++ src/App2Class.js
@@ -9,11 +9,26 @@ class App2Class extends React.Component {
         this.state = {
             todos: todosData
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(id){
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
     }
 
     render() {
         // different format when using state
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
 
         return (
             <div>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,10 @@ import React from "react"
 function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.item.completed} />
+      <input type="checkbox" checked={props.item.completed} 
+      // empty function that calls props.handleChange and passes the ID to it (ID of the item that was clicked)
+      onChange={() => props.handleChange(props.item.id)}
+      />
       <p>{props.item.text}</p>
     </div>
   )
