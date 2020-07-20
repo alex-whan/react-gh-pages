a442e8fe6d39d52e76ca43aab8995651d258bae3
Alexander Whan <whan.alexander@gmail.com>
2020-07-20 14:57:56 -0700

    Simplied log in/out function to use ternaries on buttons and logic

--- src/App7Login.js
+++ src/App7Login.js
@@ -6,10 +6,16 @@ Challenge:
 
 Given a stateless functional component:
 1. Follow the steps necessary to add state to it,
+    // class-based component
+    // constructor method
 2. Have state keep track of whether the user is logged in or not
+    // isLoggedIn: Boolean (true or false)
 3. Add a button that logs the user in/out
+    // event listener (onClick)
     a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
+        // Conditional Rendering
 4. Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not.
+    // Conditional Rendering
 */
 
 // Either: {Component} above and Component only below
@@ -23,6 +29,8 @@ class App7Login extends Component {
         this.logIn = this.logIn.bind(this);
     }
 
+    // need to know what the previous state was in this case
+    // really good/concise way to flip a Boolean to whatever the opposite is
     logIn() {
         this.setState(prevState => {
             return {
@@ -38,23 +46,13 @@ class App7Login extends Component {
     }
 
     render() {
-        let wordDisplay;
-        this.state.isLoggedIn ? wordDisplay = "in" : wordDisplay = "out";
-
-        if(this.state.isLoggedIn) {
-            return (
-                <div>
-                    <h1>You are logged {wordDisplay}!</h1>
-                    <button onClick={this.logIn}>Log Out</button>
-                </div>
-            )
-        }
-        
+        let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN"
+        let displayText = this.state.isLoggedIn ? "in" : "out";
         return (
-            <div>
-                <h1>You are logged {wordDisplay}!</h1>
-                <button onClick={this.logIn}>Log In</button>
-            </div>
+                <div>
+                    <h1>You are logged {displayText}!</h1>
+                    <button onClick={this.logIn}>{buttonText}</button>
+                </div>
         )
     }
 }
